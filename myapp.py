42be43db8240a167c7965bcab356e5e265d75b90
alecmarkmorris <Alecmarkmorris@gmail.com>
2024-08-26 00:48:37 -0700

    finished the PDF to DOCX convter

--- myapp.py
+++ myapp.py
@@ -39,17 +39,3 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
-#def app(environ, start_response):
- #       data = b"This is the home page\n"
-  #      start_response("200 OK", [
-   #         ("Content-Type", "text/plain"),
-    #        ("Content-Length", str(len(data)))
-     #   ])
-       # return iter([data])
